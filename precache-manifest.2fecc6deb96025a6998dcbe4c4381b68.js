49507351b7a0e99c9dc462c36e3bf57aa679aee9
Ram <ramdevukd@gmail.com>
2019-11-16 22:46:38 +0530

    Updates

---
+++ precache-manifest.2fecc6deb96025a6998dcbe4c4381b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5e1b19d87709ec0961f489d49dee4a",
+    "url": "/markk-assignment/index.html"
+  },
+  {
+    "revision": "9cbcb86c861898482174",
+    "url": "/markk-assignment/static/css/main.93a40829.chunk.css"
+  },
+  {
+    "revision": "0b9b9027de7d1e3a2b50",
+    "url": "/markk-assignment/static/js/2.ab7aca42.chunk.js"
+  },
+  {
+    "revision": "9cbcb86c861898482174",
+    "url": "/markk-assignment/static/js/main.863032c8.chunk.js"
+  },
+  {
+    "revision": "d03dddb6f7630c023bc6",
+    "url": "/markk-assignment/static/js/runtime-main.b9771819.js"
+  },
+  {
+    "revision": "203eac9d4c0e4ca45592f0abc83f5261",
+    "url": "/markk-assignment/static/media/nope.203eac9d.png"
+  }
+]);
