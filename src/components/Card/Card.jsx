bef3408262423ab0f0ef0f086f693b5badd89921
Ram <ramdevukd@gmail.com>
2019-11-17 17:49:42 +0530

    alt prop to img added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,42 +4,44 @@ import "./Card.css";
 class Card extends React.Component {
   state = {};
   render() {
-    console.log(this.props.id);
     return (
       <div className="card">
         <img
           className="back-img"
           src={`https://picsum.photos/id/${this.props.id}/200/300`}
+          alt="place"
         />
         {this.props.id % 2 === 0 ? (
           <img
             className="reaction"
             src={require("../../assets/img/dope.png")}
+            alt="dope"
           />
         ) : (
           <img
             className="reaction"
             src={require("../../assets/img/nope.png")}
+            alt="nope"
           />
         )}
         <div className="avatar">
-          <img src="https://i.pravatar.cc/50" />
+          <img src="https://i.pravatar.cc/50" alt="avatar" />
         </div>
         <h1>
-          <img src={require("../../assets/img/cam.png")} />
+          <img src={require("../../assets/img/cam.png")} alt="cam" />
           <span>3</span>
         </h1>
         <div className="feedback">
           <div className="heart">
-            <img src={require("../../assets/img/heart.png")} />
+            <img src={require("../../assets/img/heart.png")} alt="heart" />
             <span>2</span>
           </div>
           <div className="cmnt">
-            <img src={require("../../assets/img/cmnt.png")} />
+            <img src={require("../../assets/img/cmnt.png")} alt="comment" />
             <span>2</span>
           </div>
           <div className="share">
-            <img src={require("../../assets/img/share.png")} />
+            <img src={require("../../assets/img/share.png")} alt="share" />
             <span>3</span>
           </div>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <img src={require("../../assets/img/markk.png")} />
+        <img src={require("../../assets/img/markk.png")} alt="Markk" />
       </div>
     );
   }
